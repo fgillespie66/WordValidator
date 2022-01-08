43e26bdc090a71f0dad157cdd497038f5f3accc6
Mark Gillespie <mark.gillespie81@gmail.com>
2022-01-07 22:37:16 -0800

    pay attention to route ordering

--- src/main.js
+++ src/main.js
@@ -12,7 +12,9 @@ Vue.use(VueRouter);
 export const router = new VueRouter({
   routes: [
     { path: "/", component: WordValidator, props: true },
-    { path: "/Anagrammer", component: Anagrammer },
+    { path: "/Anagrammer", component: Anagrammer, props: true },
+    { path: "/Anagrammer/:queryWord", component: Anagrammer, props: true },
+    /* must come after other paths */
     { path: "/:queryWord", component: WordValidator, props: true },
     { path: "*", component: PageNotFound, props: true },
   ],
